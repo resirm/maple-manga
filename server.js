d91e99a5901214644839c65d7d7a5f8148cb33ac
chenye <cy@no.com>
2019-06-02 20:40:02 +0800

    完成注册

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+'use strict';
 var express = require('express')
 var bodyParser = require("body-parser"); 
 var app = express()
 var path = require('path')
 var cheerio = require('cheerio');
-var http = require('https');
+var https = require('https');
 var session = require('express-session');
-var mysql = require('mysql')
+
+const mysql = require('mysql')
 var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'wsn',
-  database: 'maple',
+    host: 'localhost',
+    user: 'root',
+    password: 'wsn',
+    database: 'maple',
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(express.static(__dirname + '/res'));
+app.use(express.static(path.join(__dirname, 'res')));
 app.use(session({
   secret :  'secret', // 对session id 相关的cookie 进行签名
   resave : true,
@@ -31,7 +33,20 @@ app.get('/' , function(req,res){
     var usr = req.query.usr;
     console.log(usr);
     req.session.userName = usr;
-    res.render('search',{usr:usr});
+    // sql1: select m.* from subscription as sub join user as u join manga as m where sub.user_id = u.user_id 
+    //       and u.user_name = 'ta78na' and sub.manga_id = m.manga_id;
+    // sql2: select * from manga as m where m.manga_id in (select sub.manga_id from subscription as sub join user as u where  //       sub.user_id = u.user_id and u.user_name = 'ta78na');
+    // use sql2;
+    const query_subscription = `SELECT * FROM manga AS m WHERE m.manga_id IN (SELECT sub.manga_id FROM subscription AS sub JOIN user AS u WHERE sub.user_id = u.user_id AND u.user_name = '${ usr }');`;
+    let mangas;
+    con.query(query_subscription, (err, ress, fields) => {
+      if(err){
+        console.log(`Error occurred when trying to get subscription information from database: ${err.message}`);
+        return;
+      }
+      mangas = ress;
+      res.render('search',{usr, mangas});
+    });
 });
 
 app.post('/result' , function(req,res){
@@ -44,61 +59,92 @@ app.post('/result' , function(req,res){
       if(err){
         console.log('[QUERY ERROR] - ',err.message);
         return;
-       } 
-       if( resss.length == 0){ //没有则插入
+        }
+        if( resss.length == 0){ //没有则插入
           let mangaaddsql = 'insert into manga(manga_id, manga_name, url, cover_url, update_time) values(null,?,?,?,?)';
           let mangapara = [mname, mlink, mcover,"time"];
           con.query(mangaaddsql,mangapara, function (err, re){
             if(err){
-                console.log('[INSERT ERROR] - ',err.message);
-              return;
-            }     
-          });
-       }
-       //获取漫画id
-       console.log(mname);
-        let qmanga_id = 'select manga_id from manga where manga_name="' + mname + '"';
-        con.query(qmanga_id, (err, ress) => {
-      if(err){
-        console.log('[QUERY ERROR] - ',err.message);
-        return;
-       } 
-       console.log(ress);
-       let mangaid =  ress[0].manga_id;
-
-       //获取用户id
-       let quser = 'select user_id from user where user_name="' + username + '"';
-       con.query(quser, (err, result) => {
-         if(err){
-           console.log('[QUERY ERROR] - ',err.message);
-           return;
-          } 
-           let usrid = result[0].user_id;
-
-           //插入订阅
-           let subaddsql = 'insert into subscription (user_id, manga_id, seen_time) values(?,?,?)';
-           let subpara = [usrid, mangaid, "test"];
-           con.query(subaddsql,subpara, (err, res) => {
-            if(err){
               console.log('[INSERT ERROR] - ',err.message);
               return;
-             } 
-           } );
-       });
-    });
+            }
+          });
+        }
+        //获取漫画id
+        console.log(mname);
+        let qmanga_id = 'select manga_id from manga where manga_name="' + mname + '"';
+        con.query(qmanga_id, (err, ress) => {
+          if(err){
+            console.log('[QUERY ERROR] - ',err.message);
+            return;
+           }
+           console.log(ress);
+           let mangaid =  ress[0].manga_id;
+         
+           //获取用户id
+           let quser = 'select user_id from user where user_name="' + username + '"';
+           con.query(quser, (err, result) => {
+             if(err){
+               console.log('[QUERY ERROR] - ',err.message);
+               return;
+              } 
+              // debug
+              console.table(result);
+              let usrid = result[0].user_id;
+            
+              //插入订阅
+              let subaddsql = 'insert into subscription (user_id, manga_id, seen_time) values(?,?,?)';
+              let subpara = [usrid, mangaid, "test"];
+              con.query(subaddsql,subpara, (err, res) => {
+                if(err){
+                  console.log('[INSERT ERROR] - ',err.message);
+                  return;
+                } 
+              });
+            });
+        });
     });
     res.send("nice");
 });
 
+app.get('/regist', function(req, res) {
+  res.render('regist');
+});
+
+app.post('/registHandle', function(req, res) {
+  let uname = req.body.username;
+  let uemail = req.body.email;
+  let upass = req.body.password;
+
+  if (upass != "zyfsmalldick")
+  res.send("SB, GET OUT OF HERE!");
+  else{
+    let newuser = `insert into user values(null,"${uname}","${uemail}")`;
+    con.query(newuser, (err, ress) => {
+      if(err){
+        console.log(`Error occurred when trying to insert new user: ${err.message}`);
+        res.send("失败，用户名重复");
+        return;
+      }
+        res.send("注册成功，baby");
+      console.log(`new user: "${uname}"`);
+    });
+  }
+  
+});
+
+
+
 app.post('/search', function(req, res) {
   var para = req.body.search;
   console.log(req.session.userName);
+  var usrname = req.session.userName;
   var mangas = [];
   var m = res;
   var url = 'https://www.manhuafen.com/search/?keywords='+encodeURI(para);
   console.log(url);
   
-  http.get(url, function(res){
+  https.get(url, function(res){
     var chunks = [];
     var size = 0;
     res.on('data', function(chunk){
@@ -119,17 +165,62 @@ app.post('/search', function(req, res) {
             mangas.push(manga);
             // console.log(manga);
         });
-        m.render('result', {mangas:mangas});
-        
+        m.render('result', {mangas:mangas, usrname});
     });
-    
   });
 });
+
 var server = app.listen(3000, function () {
- 
     var host = server.address().address;
     var port = server.address().port;
-   
-    console.log("应用实例，访问地址为 http://%s:%s", host, port);
-   
-  })
+    console.log(`应用实例，访问地址为 http://${ host==='::'?'localhost':host}:${port}`);
+    //console.log(`Current path: ${__dirname}`);
+})
+
+let update_checker = setInterval(() => {
+  if(con.state !== 'authenticated'){
+    con.connect();
+  }
+  console.log(con.state);
+  const QUERY = 'SELECT * FROM manga;';
+  let mangas;
+  con.query(QUERY, (err, res, fields) => {
+    if(err){
+      console.log(`Error occurred when trying to get manga information from database: ${err.message}`);
+      return;
+    }
+    mangas = res;
+    mangas.forEach(manga => {
+      https.get(manga.url, function(res){
+        var chunks = [];
+        var size = 0;
+        res.on('data', function(chunk){
+            chunks.push(chunk);
+            size += chunk.length;
+        });
+        
+        res.on('end', function(){
+            var data = Buffer.concat(chunks, size);
+            var html = data.toString();
+            var $ = cheerio.load(html);
+            
+            let update_time = $("span.zj_list_head_dat").text();
+            console.log(`update_time: ${update_time}`);
+            if (update_time !== manga.update_time){
+              const UPDATE = `UPDATE manga set update_time = "${update_time}" WHERE manga_id = ${manga.manga_id};`;
+              console.log(UPDATE);
+              console.log(`${manga.manga_name} updated at: ${update_time}.`)
+              console.log(update_time);
+              con.query(UPDATE, (err, res, fields) => {
+                if(err){
+                  console.log(`Error occurred when trying to get manga information from database: ${err.message}`);
+                  return;
+                }
+                console.table(res);
+              })
+            }
+          });
+        });
+    });
+  });
+}, 3600000);
